80c000aa80186a295b6723bd02700e21ef813472
cvalerie1102 <90990858+cvalerie1102@users.noreply.github.com>
2024-11-17 21:33:33 -0600

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,9 @@ import socket
 import sys
 import time
 
+
 def main():
-    # Check for command-line arguments
+   
     if len(sys.argv) != 3:
         print("Usage: python client.py <hostname> <port>")
         sys.exit(1)
@@ -11,7 +12,7 @@ def main():
     hostname = sys.argv[1]
     port = int(sys.argv[2])
 
-    # Create a UDP socket
+   
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.settimeout(1)  # Set a 1-second timeout for responses
 
@@ -24,6 +25,7 @@ def main():
             print(f"{i} : sent PING...", end=' ')
             client_socket.sendto(message, server_address)  # Send the message to the server
 
+            
             # Wait for the response
             data, _ = client_socket.recvfrom(1024)
             print(f"received {data}")
@@ -31,6 +33,7 @@ def main():
             print("Timed Out")
 
     client_socket.close()
+
 
 if __name__ == "__main__":
     main()
